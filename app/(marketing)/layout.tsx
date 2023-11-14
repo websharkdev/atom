cb741411664e71184c78c274fe18a0af3ad7922f
Oleksii Bortnytskyi ⚛ <47607282+websharkdev@users.noreply.github.com>
2023-11-14 15:30:30 +0100

    Update layout.tsx

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -9,7 +9,7 @@ const MarketingLayout = ({ children }: Props) => {
   return (
     <div className="h-full dark:bg-[#1f1f1f]">
       <Navbar />
-      <main className="h-full pt-40">{children}</main>
+      <main className="h-full pt-40 dark:bg-[#1f1f1f]">{children}</main>
     </div>
   );
 };
